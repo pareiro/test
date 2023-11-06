e40723244551b8ae58cb5e3b3f266b9e60f42c47
pareiro <pareiro77@yandex.ru>
2023-11-06 09:02:10 +0000

    Добавлен код для создания и вывода строки

---
+++ test.py
@@ -0,0 +1,6 @@
+# Создаем переменную типа string
+test_string = "pereiro_loves_python"
+
+# Выводим значение переменной и ее тип
+print("Значение переменной test_string:", test_string)
+print("Тип переменной test_string:", type(test_string))
